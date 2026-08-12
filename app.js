2a23801cabb2df99170167737f9c46ff726a6f70
EarthisChillin <129577305+EarthisChillin@users.noreply.github.com>
2026-08-11 22:50:33 -0500

    Update app.js

--- app.js
+++ app.js
@@ -893,10 +893,7 @@ For inspiration, clarifying previous discoveries, knowledge on textures/archives
 
 <span class="credit-name">-ngh-</span>
 hey ngh!!! ngh please!!! help!!! ngh... sigh...
-(massive help in understanding certain tools or workflows, i would annoy him into rage when i myself was raging over making this, tough love when needed)
-
-<span class="credit-name">-Akela-</span>
-Support. Gotta love those that give you encouragement. Plus I see him nearly everywhere I go!`;
+(massive help in understanding certain tools or workflows, i would annoy him into rage when i myself was raging over making this, tough love when needed);
 
 function openCredits() {
   el.creditsBody.innerHTML = CREDITS_HTML;
